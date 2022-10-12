28c61085434a3c96f623160e5e5a20d990a4ebdc
jeffreyvavasour <93945866+jeffreyvavasour@users.noreply.github.com>
2022-10-12 16:38:25 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+// selectors
+const primaryHeader = document.querySelector('.primary-header');
+const mobileNavToggle = document.querySelector('.mobile-nav-toggle');
+const primaryNav = document.querySelector('.primary-nav');
+
+// EVENT LISTENERS
+// nav toggle
+mobileNavToggle.addEventListener('click', function() {
+    const dataVisible = primaryNav.getAttribute('data-visible');
+    if (dataVisible === 'false') {
+        primaryNav.setAttribute('data-visible', 'true');
+        mobileNavToggle.style.backgroundImage = 'url(./assets/images/icon-close.svg)';
+    } else {
+        primaryNav.setAttribute('data-visible', 'false');
+        mobileNavToggle.style.backgroundImage = 'url(./assets/images/icon-hamburger.svg)';
+    }
+})
+
+document.addEventListener('click', (e) => {
+    if (e.target.id !== 'primary-nav' && e.target.id !== 'nav-toggle' && e.target.id !== 'btn-top-of-page') {
+        primaryNav.setAttribute('data-visible', 'false');
+        mobileNavToggle.style.backgroundImage = 'url(./assets/images/icon-hamburger.svg)';
+    };
+});
