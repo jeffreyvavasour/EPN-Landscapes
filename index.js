1e4a9646a9a20a4016045a40b0902d4258c996b3
jeffreyvavasour <93945866+jeffreyvavasour@users.noreply.github.com>
2022-10-13 17:59:58 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const primaryHeader = document.querySelector('.primary-header');
 const mobileNavToggle = document.querySelector('.mobile-nav-toggle');
 const primaryNav = document.querySelector('.primary-nav');
 
+const btnSliderLeft = document.querySelector('.btn-slider-left');
+const btnSliderRight = document.querySelector('.btn-slider-right');
+const reviewsContentSlides = document.querySelectorAll('.reviews-content-slide');
+
 // EVENT LISTENERS
 // nav toggle
 mobileNavToggle.addEventListener('click', function() {
@@ -22,3 +26,33 @@ document.addEventListener('click', (e) => {
         mobileNavToggle.style.backgroundImage = 'url(./assets/images/icon-hamburger.svg)';
     };
 });
+
+// review slides
+reviewsContentSlides.forEach((slide, index) => {
+    slide.style.left = `${index}00%`;
+})
+
+// reviews slider
+let count = 0;
+
+btnSliderRight.addEventListener('click', function() {
+    count++;
+    carousel()
+})
+btnSliderLeft.addEventListener('click', function() {
+    count--;
+    carousel()
+})
+
+// FUNCTIONS
+function carousel() {
+    if(count >= reviewsContentSlides.length) {
+        count = 0;
+    }
+    if(count <= -1) {
+        count = reviewsContentSlides.length - 1;
+    }
+    reviewsContentSlides.forEach(slide => {
+        slide.style.transform = `translateX(-${count}00%)`;
+    })
+}
